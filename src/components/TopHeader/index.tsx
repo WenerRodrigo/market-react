7da074d493939b2933da52e6ddad626b99ca6431
WenerRodrigo <wener19.wr@gmail.com>
2024-03-04 22:19:25 -0300

    chore: created component topHeader

---
+++ src/components/TopHeader/index.tsx
@@ -0,0 +1,12 @@
+
+const TopHeader = () => {
+  return (
+    <div className='flex justify-center items-center w-full'>
+        <div className='flex items-center justify-center bg-primary w-full h-10'>
+            <span className='text-center text-white text-md'>Frete grátis para compras acima de 199</span>
+        </div>
+    </div>
+  )
+}
+
+export default TopHeader
