66231bccb54de2f35bf39809438b6948ec715148
WenerRodrigo <wener19.wr@gmail.com>
2024-03-04 22:22:11 -0300

    chore: created component login

---
+++ src/components/login/index.tsx
@@ -0,0 +1,13 @@
+import { MdAccountCircle } from "react-icons/md";
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <MdAccountCircle />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
