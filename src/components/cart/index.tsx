8a66363b77b88a10fdda524ba31765ec04dc7072
WenerRodrigo <wener19.wr@gmail.com>
2024-03-05 22:35:42 -0300

    chore: fix cart

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -22,14 +22,11 @@ const Cart = () => {
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
-
   return (
     <div className="flex items-center justify-between cursor-pointer ml-2 relative">
       <div className="flex items-center relative" onClick={toggleMenu}>
         <BsCart className="flex items-center justify-center text-2xl text-white font-bold" />
-        {!isMobile && (
-          <span className="text-sm text-white absolute -top-1 -right-2 bg-red-500 px-1 rounded-full">0</span>
-        )}
+        <span className="text-sm text-white absolute -top-1 -right-2 bg-red-500 px-1 rounded-full">0</span>
       </div>
       {isMenuOpen && (
         <div className={`absolute ${isMobile ? 'top-full' : 'top-0'} -mt-1 ${isMobile ? 'w-full max-h-screen overflow-y-scroll' : ''}`}>
