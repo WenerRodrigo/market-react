a0ac47acb358d42e290ebbe920517f9287bd98d2
WenerRodrigo <wener19.wr@gmail.com>
2024-03-09 16:20:02 -0300

    chore: fix

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -5,13 +5,12 @@ export const Main = () => {
     <div className="hidden md:flex items-center justify-center py-4">
       <div className="max-w-screen-lg mx-auto">
         <div className="flex justify-between">
-          <a href="/cell-phones" className="px-4 py-2 mr-4 hover:text-gray-300">Celulares</a>
+          <a href="/cellphones" className="px-4 py-2 mr-4 hover:text-gray-300">Celulares</a>
           <a href="#" className="px-4 py-2 mr-4 hover:text-gray-300">Tablets</a>
           <a href="#" className="px-4 py-2 mr-4 hover:text-gray-300">Computadores</a>
-          <a href="#" className="px-4 py-2 mr-4 hover:text-gray-300">Casa Mesa Banho</a>
+          <a href="#" className="px-4 py-2 mr-4 hover:text-gray-300">Acessórios</a>
           <a href="#" className="px-4 py-2 mr-4 hover:text-gray-300">Ofertas</a>
           <a href="#" className="px-4 py-2 mr-4 hover:text-gray-300">Casa</a>
-          <a href="#" className="px-4 py-2 hover:text-gray-300">Variedades</a>
         </div>
       </div>
     </div>
--- src/screens/cellPhones/index.tsx
+++ src/screens/cellPhones/index.tsx
@@ -14,46 +14,84 @@ interface IProduct {
 export const CellPhones: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<IProduct[]>([]);
-
-  const fetchData = async () => {
-    setIsLoading(true);
-    try {
-      const response = await axios.get(
-        "https://api.mercadolibre.com/sites/MLB/search?q=celular"
-      );
-      setData(response.data.results);
-    } catch (error) {
-      console.error("Erro ao buscar dados da API:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // const [installmentPrice, setInstallmentPrice] = useState<number | null>(null);
+  // const numberOfInstallments = 12;
 
   useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          "https://api.mercadolibre.com/sites/MLB/search?q=celular"
+        );
+        setData(response.data.results);
+        // calculateInstallment(response.data.results);
+      } catch (error) {
+        console.error("Erro ao buscar dados da API:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchData();
   }, []);
+
+  // const calculateInstallment = (products: IProduct[]) => {
+  //   if (products.length > 0) {
+  //     const totalPrice = products.reduce(
+  //       (accumulator, product) => accumulator + product.price,
+  //       0
+  //     );
+  //     const installmentValue = totalPrice / numberOfInstallments;
+  //     setInstallmentPrice(installmentValue);
+  //   }
+  // };
+
+  const formatPrice = (price: number) => {
+    return price.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
 
   return (
     <div className="flex flex-col items-center justify-center py-4">
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 w-full max-w-6xl px-4">
         {data.map((product) => (
-          <div key={product.id} className="border p-4 flex flex-col rounded-md hover:shadow-lg">
-            <img src={product.thumbnail} alt={product.title} className="mb-2 object-cover h-auto w-full" />
-            <div className="flex flex-col justify-between h-32"> {/* Definindo uma altura fixa para a área de descrição */}
+          <div
+            key={product.id}
+            className="border p-4 flex flex-col rounded-md hover:shadow-lg"
+          >
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              className="mb-2 object-cover h-auto w-full"
+            />
+            <div className="flex flex-col justify-between h-32">
               <div>
                 <h2 className="text-sm font-bold mb-2">{product.title}</h2>
                 {product.discountedPrice && (
                   <p className="text-gray-700 mb-2">
-                    Discounted Price: {product.discountedPrice}
+                    Discounted Price: {formatPrice(product.discountedPrice)}
                   </p>
                 )}
                 {product.model && (
                   <p className="text-gray-700 mb-2">Model: {product.model}</p>
                 )}
               </div>
-              <p className="text-black text-sm mb-2">R$: {product.price}</p> {/* Preço fora do contêiner flexível */}
+              <p className="text-black text-sm mb-2">
+                {formatPrice(product.price)}
+              </p>
             </div>
-            <button type="button" className="bg-green-600 text-white rounded-md py-2">
+            {/* {installmentPrice && (
+              <p className="text-sm text-gray-700 mb-2">
+                ou {numberOfInstallments}x de {formatPrice(installmentPrice)}
+              </p>
+            )} */}
+            <button
+              type="button"
+              className="bg-green-600 text-white rounded-md py-2"
+            >
               Comprar
             </button>
           </div>
