c0ff6d40b58e4bec46fe01f0dc87111a0d089067
WenerRodrigo <wener19.wr@gmail.com>
2024-03-09 18:24:56 -0300

    chore: ajusted menu Mobile

--- src/screens/header/index.tsx
+++ src/screens/header/index.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
 
   return (
     <div className="flex items-center justify-center bg-secondary py-2 z-50">
-      <div className="flex items-center w-4/5">
+      <div className="flex items-center w-4/5 relative">
         <h1 className="text-2xl text-white font-bold hidden sm:flex">Market</h1>
         <span className="sm:hidden">
           {isMobileMenuOpen ? (
@@ -43,13 +43,13 @@ export const Header = () => {
         <Cart />
 
         {isMobileMenuOpen && (
-          <div className="fixed inset-0 bg-gray-800 bg-opacity-50">
-            <div className="flex flex-col bg-white w-4/5 h-full relative">
+          <div className="fixed inset-0 bg-gray-800 bg-opacity-50 z-50">
+            <div className="flex flex-col bg-white w-4/5 h-full relative z-50">
               <IoCloseOutline
-                className="text-2xl text-black self-end mr-4 mt-4 cursor-pointer"
+                className="text-2xl text-black self-end mr-4 mt-4 cursor-pointer z-50"
                 onClick={closeMobileMenu}
               />
-              <div className="absolute flex items-center justify-center flex-row mt-4 px-4">
+              <div className="absolute flex items-center justify-center flex-row mt-4 px-4 z-50">
                 <span className="flex items-center flex-col mr-2">
                   <MdAccountCircle className="text-xl flex items-center" />
                 </span>
@@ -58,20 +58,20 @@ export const Header = () => {
                 </a>
               </div>
               <span className="block border-b border-gray-300 w-full mt-4"></span>
-              <div className="px-2 mt-4">
+              <div className="px-2 mt-4 z-50">
                 <MenuItem text="Celulares" />
                 <MenuItem text="Tablets" />
                 <MenuItem text="Computadores" />
                 <MenuItem text="Acessórios" />
                 <MenuItem text="Ofertas" />
                 <MenuItem text="Casa Mesa Banho" />
-                <div className="mt-4 flex items-center justify-between">
+                <div className="mt-4 flex items-center justify-between z-50">
                   <a href="#" className="text-red-500">
                     Sair
                   </a>
                   <IoIosArrowForward />
                 </div>
-                <span className="block border-b border-gray-300 w-full mt-4"></span>
+                <span className="block border-b border-gray-300 w-full mt-4 z-50"></span>
               </div>
             </div>
           </div>
