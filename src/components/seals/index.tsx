ce2f2af69edeaf8bc525a2785d5c588112a0a68d
WenerRodrigo <wener19.wr@gmail.com>
2024-03-19 22:06:10 -0300

    chore: fix

--- src/components/seals/index.tsx
+++ src/components/seals/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Seals = () => {
   return (
-    <div className='flex items-center w-4/5 bg-blue-700 top-36 lg:bottom-48 absolute justify-center rounded-md'>
+    <div className='flex items-center w-4/5 bg-blue-700 top-36 lg:bottom-42 absolute justify-center rounded-md'>
         <span className='text-center text-sm text-white'>Frete grátis</span>
     </div>
   );
