778f64c686283af126f58f943f79efff7133b308
WenerRodrigo <wener19.wr@gmail.com>
2024-03-20 21:20:58 -0300

    chore: created component loading

--- src/screens/cellPhones/index.tsx
+++ src/screens/cellPhones/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { Seals } from "../../components/seals";
+import { LazyLoading } from "../../components/lazyLoading";
 
 interface IProduct {
   id: string;
@@ -98,79 +99,86 @@ export const CellPhones = ({ searchQuery }: ICellPhonesProps) => {
 
   return (
     <div className="flex flex-col items-center justify-center py-4">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 w-full max-w-6xl px-4">
-        {data.map((product) => (
-          <div
-            key={product.id}
-            className="border p-4 flex flex-col rounded-md hover:shadow-lg relative"
-          >
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-              className="mb-2 object-cover h-auto w-full"
-            />
-            <div className="flex flex-col justify-between h-32">
-              <div>
-                <h2 className="text-sm text-gray-600 mb-2">{product.title}</h2>
-                {product.discountedPrice && (
-                  <p className="text-gray-700 mb-2">
-                    Discounted Price: {formatPrice(product.discountedPrice)}
-                  </p>
-                )}
-                {product.model && (
-                  <p className="text-gray-700 mb-2">Model: {product.model}</p>
-                )}
-                <Seals />
+      {isLoading ? (
+        <div className="w-full h-full flex items-center justify-center">
+          <LazyLoading />
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 w-full max-w-6xl px-4">
+          {data.map((product) => (
+            <div
+              key={product.id}
+              className="border p-4 flex flex-col rounded-md hover:shadow-lg relative"
+            >
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+                className="mb-2 object-cover h-auto w-full"
+              />
+              <div className="flex flex-col justify-between h-32">
+                <div>
+                  <h2 className="text-sm text-gray-600 mb-2">
+                    {product.title}
+                  </h2>
+                  {product.discountedPrice && (
+                    <p className="text-gray-700 mb-2">
+                      Discounted Price: {formatPrice(product.discountedPrice)}
+                    </p>
+                  )}
+                  {product.model && (
+                    <p className="text-gray-700 mb-2">Model: {product.model}</p>
+                  )}
+                  <Seals />
+                </div>
+                <p className="text-black text-sm mb-2">
+                  {formatPrice(product.price)}
+                </p>
               </div>
-              <p className="text-black text-sm mb-2">
-                {formatPrice(product.price)}
-              </p>
-            </div>
-            {selectedCardId !== product.id && (
-              <button
-                type="button"
-                className="bg-green-600 text-white rounded-md py-2 hover:bg-green-700"
-                onClick={() => handleBuyClick(product.id)}
-              >
-                Comprar
-              </button>
-            )}
-            {selectedCardId === product.id && (
-              <div className="flex items-center justify-between mt-2 text-lg relative">
-                {productQuantity[product.id] === 1 && (
-                  <button
-                    className="flex items-center justify-center border w-10 rounded-md bg-gray-100"
-                    onClick={() => handleRemoveProduct(product.id)}
-                  >
-                    <button className="flex items-center justify-center border w-10 h-7 rounded-md border-none">
-                      <FaRegTrashAlt className="text-red-600" />
-                    </button>
-                  </button>
-                )}
-                {productQuantity[product.id] &&
-                  productQuantity[product.id] > 1 && (
+              {selectedCardId !== product.id && (
+                <button
+                  type="button"
+                  className="bg-green-600 text-white rounded-md py-2 hover:bg-green-700"
+                  onClick={() => handleBuyClick(product.id)}
+                >
+                  Comprar
+                </button>
+              )}
+              {selectedCardId === product.id && (
+                <div className="flex items-center justify-between mt-2 text-lg relative">
+                  {productQuantity[product.id] === 1 && (
                     <button
                       className="flex items-center justify-center border w-10 rounded-md bg-gray-100"
-                      onClick={() => handleDecrementQuantity(product.id)}
+                      onClick={() => handleRemoveProduct(product.id)}
                     >
-                      -
+                      <button className="flex items-center justify-center border w-10 h-7 rounded-md border-none">
+                        <FaRegTrashAlt className="text-red-600" />
+                      </button>
                     </button>
                   )}
-                <span className="text-md flex-1 text-center">
-                  {productQuantity[product.id] || 0}
-                </span>
-                <button
-                  className="flex items-center justify-center border w-10 rounded-md bg-gray-100"
-                  onClick={() => handleIncrementQuantity(product.id)}
-                >
-                  +
-                </button>
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-      {isLoading && <p className="text-center">Carregando...</p>}
+                  {productQuantity[product.id] &&
+                    productQuantity[product.id] > 1 && (
+                      <button
+                        className="flex items-center justify-center border w-10 rounded-md bg-gray-100"
+                        onClick={() => handleDecrementQuantity(product.id)}
+                      >
+                        -
+                      </button>
+                    )}
+                  <span className="text-md flex-1 text-center">
+                    {productQuantity[product.id] || 0}
+                  </span>
+                  <button
+                    className="flex items-center justify-center border w-10 rounded-md bg-gray-100"
+                    onClick={() => handleIncrementQuantity(product.id)}
+                  >
+                    +
+                  </button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
