a63f6a52007b1dae7a661b1ef34d69ba46d33c09
WenerRodrigo <wener19.wr@gmail.com>
2024-03-23 15:56:35 -0300

    chore: fix

--- src/components/dropdownMenu/index.tsx
+++ src/components/dropdownMenu/index.tsx
@@ -44,7 +44,7 @@ export const DropDownMenu = () => {
           >
             <div className="py-1" role="none">
               <a
-                href="/register"
+                href="#"
                 className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-300"
                 role="menuitem"
                 tabIndex={-1}
